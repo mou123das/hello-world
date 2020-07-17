55431526434595e70a7081f77cfdcaf927eaf523
Moumita Das <45044600+mou123das@users.noreply.github.com>
2020-07-17 05:52:10 +0530

    Update hello world.java

--- hello world.java
+++ hello world.java
@@ -1,7 +1,15 @@
 class FIRST_REPOSITORY
 {
-public void print()
-{
-System.out.println("hello-world");
-}
+  
+ public void print()
+ {
+  System.out.println("Hello World!!");
+ }
+  
+ public static void main(String args[])
+ {
+   FIRST_REPOSITORY obj=new FIRST_REPOSITORY();
+   obj.print();
+ }
+ 
 }
